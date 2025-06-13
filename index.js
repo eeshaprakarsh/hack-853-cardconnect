d7ffc162f71c3a5024b669ab35bae434a5c755c3
Eesha Prakarsh <eprakarsh@cardconnect.com>
2025-06-13 10:58:36 -0400

    cors origin added

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://your-frontend.onrender.com'
+    'https://hack-853-cardconnect-backend.onrender.com/'
   ];
 
-  app.use(cors({
+app.use(cors({
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-  }));
-  
+}));
+
 app.use(express.json());
 
 app.use('/api/transactions', TransactionRoute);
